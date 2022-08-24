3cabeea18950489bb286a4f1f3030d5e2b733486
jason <su3g4196ta8@gmail.com>
2022-08-24 22:05:20 +0800

    enable flutter widget guide

--- lua/jason/flutter-tool.lua
+++ lua/jason/flutter-tool.lua
@@ -13,17 +13,20 @@ require('flutter-tools').setup{
     statusline = {
       -- set to true to be able use the 'flutter_tools_decorations.app_version' in your statusline
       -- this will show the current version of the flutter app from the pubspec.yaml file
-      app_version = false,
+      app_version = true,
       -- set to true to be able use the 'flutter_tools_decorations.device' in your statusline
       -- this will show the currently running device if an application was started with a specific
       -- device
-      device = false,
+      device = true,
     },
   },
+	widget_guides = {
+		enabled = true,
+	},
 	lsp = {
     color = { -- show the derived colours for dart variables
       enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
-      background = false, -- highlight the background
+      background = true, -- highlight the background
       foreground = true, -- highlight the foreground
       virtual_text = true, -- show the highlight using virtual text
       virtual_text_str = "■", -- the virtual text character to highlight
@@ -35,7 +38,7 @@ require('flutter-tools').setup{
     settings = {
       showTodos = true,
       completeFunctionCalls = true,
-      analysisExcludedFolders = {"D:/flutter"},
+      --analysisExcludedFolders = {""},
       renameFilesWithClasses = "prompt", -- "always"
       enableSnippets = true,
     }
