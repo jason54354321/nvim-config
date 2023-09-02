e3e5a5223d27b8f13d7f2be65365ed4c4d1175a3
Jason <su3g4196ta8@gmail.com>
2023-09-02 23:34:44 +0800

    improve: local-highlight colors

--- lua/jason/local-highlight.lua
+++ lua/jason/local-highlight.lua
@@ -3,6 +3,9 @@ require('local-highlight').setup({
 	-- file_types = { 'python', 'cpp' },
 	-- OR attach to every filetype except:
 	disable_file_types = { 'groovy', 'xml' },
-	hlgroup = 'Visual',
-	cw_hlgroup = nil,
+	hlgroup = 'LocalHighLight',
+	cw_hlgroup = 'LocalHighLight',
 })
+
+-- vim.api.nvim_set_hl(0, 'LocalHighLight', { fg = "#282b30", bg = "#acadb1" })
+vim.api.nvim_set_hl(0, 'LocalHighLight', { bg = "#373b40" })
