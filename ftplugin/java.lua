b5350554963d8f9ae6b1c4fb79bb4ee1aafb28c9
Jason <su3g4196ta8@gmail.com>
2023-09-10 12:57:34 +0800

    feat(jdtls): fix win/unix os specify path

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -4,7 +4,15 @@ local config_dir = jdtls_dir .. "/config_win"
 local path_to_plugins = jdtls_dir .. "/plugins/"
 local path_to_lombok = jdtls_dir .. "/lombok.jar"
 local path_to_jar = path_to_plugins .. "org.eclipse.equinox.launcher_1.6.500.v20230622-2056.jar"
-local java_home = "C:/Program Files/Java/jdk-17"
+-- local java_home = "C:/Program Files/Java/jdk-17"
+local java_home = os.getenv("JAVA_HOME")
+
+local java_exe_or_bin
+if vim.fn.has('win32') == 1 then
+	java_exe_or_bin = java_home .. '/bin/java.exe' 
+else
+	java_exe_or_bin = java_home .. '/bin/java' 
+end
 
 local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
 local root_dir = require("jdtls.setup").find_root(root_markers)
@@ -16,16 +24,20 @@ end
 -- An example how you could accomplish that is to infer the workspace directory name from the current working directory
 local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
 local workspace_dir = vim.fn.stdpath('data') .. '/site/java/workspace-root/' .. project_name
-os.execute("mkdir " .. workspace_dir)
-
-print("HIIIIsadasd")
+print("workspace_dir:")
+print(workspace_dir)
+if vim.fn.has('win32') == 1 then
+	os.execute("mkdir " .. workspace_dir)
+else
+	os.execute("mkdir -p " .. workspace_dir)
+end
 
 -- Main Config
 local config = {
 	-- The command that starts the language server
 	-- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
 	cmd = {
-		java_home .. '/bin/java.exe', -- or '/path/to/java17_or_newer/bin/java'
+		java_exe_or_bin, -- or '/path/to/java17_or_newer/bin/java'
 		-- depends on if `java` is in your $PATH env variable and if it points to the right version.
 
 		'-Declipse.application=org.eclipse.jdt.ls.core.id1',
