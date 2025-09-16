9d2766cb0d555d41254ffe67167e2a272204c318
Gustavo Lin <gustavolin@GustavodeMacBook-Pro.local>
2025-09-16 16:40:13 +0800

    style: remove diagnostic line text

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -56,12 +56,6 @@ local function lsp_related_ui_adjust()
         [vim.diagnostic.severity.INFO] = "",
         [vim.diagnostic.severity.HINT] = "󰠠",
       },
-      linehl = {
-        [vim.diagnostic.severity.ERROR] = "Error",
-        [vim.diagnostic.severity.WARN] = "Warn",
-        [vim.diagnostic.severity.INFO] = "Info",
-        [vim.diagnostic.severity.HINT] = "Hint",
-      },
     },
   })
 end
