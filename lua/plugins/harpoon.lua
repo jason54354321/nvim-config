fb474b9f97763ab02ae818bf7b4921462313323f
gustavo <gustavolin@qnap.com>
2025-10-02 10:43:26 +0800

    keymap(harpoon): change prefix from ctrl to meta(alt)

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -6,10 +6,10 @@ return {
   keys = {
     {"<leader>a", function() require('harpoon.mark').add_file() end, desc = "Harpoon Add"},
     {"<leader>e", function() require('harpoon.ui').toggle_quick_menu() end, desc = "Harpoon Menu"},
-    {"<c-j>", function() require('harpoon.ui').nav_file(1) end, desc = "Harpoon Go File1"},
-    {"<c-k>", function() require('harpoon.ui').nav_file(2) end, desc = "Harpoon Go File2"},
-    {"<c-l>", function() require('harpoon.ui').nav_file(3) end, desc = "Harpoon Go File3"},
-    {"<c-h>", function() require('harpoon.ui').nav_file(4) end, desc = "Harpoon Go File4"}
+    {"<M-j>", function() require('harpoon.ui').nav_file(1) end, desc = "Harpoon Go File1"},
+    {"<M-k>", function() require('harpoon.ui').nav_file(2) end, desc = "Harpoon Go File2"},
+    {"<M-l>", function() require('harpoon.ui').nav_file(3) end, desc = "Harpoon Go File3"},
+    {"<M-;>", function() require('harpoon.ui').nav_file(4) end, desc = "Harpoon Go File4"}
   },
 
   config = function()
